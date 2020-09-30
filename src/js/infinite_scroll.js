09b79e750a81dfb013ea573d70b701a4c457f6eb
aersosi <aersosi@avodaq.com>
2020-09-30 11:59:55 +0200

    test

---
+++ src/js/infinite_scroll.js
@@ -0,0 +1,45 @@
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+
+    // we will add this content, replace for anything you want to add
+var wrapper, content, test;
+var more = '<div style="height:1000px; background:red;"></div>';
+
+// this is the scroll event x
+function scroller() {
+  // print relevant scroll info
+  test.innerHTML =
+    wrapper.scrollTop +
+    " + " +
+    wrapper.offsetHeight +
+    " + 100 > " +
+    content.offsetHeight;
+
+  // add more contents if user scrolled down enough
+  if (wrapper.scrollTop + wrapper.offsetHeight + 100 > content.offsetHeight) {
+    content.innerHTML += more; // NK: Here you can make an Ajax call and fetch content to append to content.innerHTML
+  }
+}
+
+
+
+
+
+
+  wrapper = document.getElementById("wrapper");
+  content = document.getElementById("content");
+  test = document.getElementById("test");
+
+  // content.innerHTML = more;
+
+  // hook the scroll handler to scroll event
+  if (wrapper.addEventListener) {
+    // NK: Works on all new browsers
+    wrapper.addEventListener("scroll", scroller, false);
+  } else if (wrapper.attachEvent) {
+    // NK: Works on old IE
+    wrapper.attachEvent("onscroll", scroller);
+  }
+});
---
+++ test.md
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/aersosi/flags-of-he-world.git
+
+git pull https://github.com/aersosi/flags-of-he-world.git master
